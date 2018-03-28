78d32b9b63116a18560745d9c70278dc1b878de2
Robert Sullivan <acethebasenji@gmail.com>
2018-03-28 09:17:25 -0400

    Add files via upload

---
+++ neo_pixel/neo_pixel.ino
@@ -0,0 +1,55 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+int whitePin = 1;
+int rgbPin = 2;
+int redPin = 3;
+int greenPin = 4;
+int bluePin = 5;
+int buttonState = 0;
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+  strip.setBrightness(30); //adjust brightness here
+  strip.show(); // Initialize all pixels to 'off'
+  pinMode(whitePin, INPUT);
+  pinMode(rgbPin, INPUT);
+  pinMode(redPin, INPUT);
+  pinMode(greenPin, INPUT);
+  pinMode(bluePin, INPUT);
+}
+
+void loop() {
+
+
+  buttonState = digitalRead(whitePin);
+  if (buttonState == HIGH) {
+    colorWipe(strip.Color(250, 250, 250), 3); // Red
+    colorWipe(strip.Color(250, 250, 250), 3); // Green
+    colorWipe(strip.Color(250, 250, 250), 3); // Blue
+  }
+  else {
+    digitalWrite(whitePin, LOW);
+  }
+}
+
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+
---
+++ simplerob/simplerob.ino
@@ -0,0 +1,37 @@
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+
+#define PIN            6
+
+
+#define NUMPIXELS      16
+
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 100; // delay for half a second
+
+void setup() {
+
+  pixels.begin(); // This initializes the NeoPixel library.
+}
+
+void loop() {
+
+
+
+  for (int i = 0; i < NUMPIXELS; i++) {
+
+
+    pixels.setPixelColor(i, pixels.Color(250, 250, 250)); // Moderately bright green color.
+
+    pixels.show(); // This sends the updated pixel color to the hardware.
+
+    delay(delayval); // Delay for a period of time (in milliseconds).
+
+  }
+}
